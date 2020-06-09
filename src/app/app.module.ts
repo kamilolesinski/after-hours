1ac3f35196373e7d8b577ddf99e26d4ad966af79
Kamil Olesiński <oles.kamil@gmail.com>
2020-06-09 23:22:32 +0200

    Sign in: ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+
 import { AppComponent } from './app.component'
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ]
 })
 export class AppModule { }
