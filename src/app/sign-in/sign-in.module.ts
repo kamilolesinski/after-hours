ec20a3a4e6a482eefd8c0b9d221617b56a81687c
Kamil Olesiński <oles.kamil@gmail.com>
2020-06-25 17:01:53 +0200

    Sign in: removing unnecessary import

--- src/app/sign-in/sign-in.module.ts
+++ src/app/sign-in/sign-in.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { AngularFireAuthModule } from '@angular/fire/auth'
 import { ReactiveFormsModule } from '@angular/forms'
 
 import { components, SignInRoutingModule } from './sign-in-routing.module'
@@ -9,7 +8,6 @@ import { FormModule } from '../form/form.module'
 @NgModule({
   declarations: components,
   imports: [
-    AngularFireAuthModule,
     CommonModule,
     FormModule,
     ReactiveFormsModule,
