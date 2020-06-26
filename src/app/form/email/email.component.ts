e840e0d1c53e388468239fecadd508164030f788
Kamil Olesiński <oles.kamil@gmail.com>
2020-06-26 12:50:57 +0200

    Sign in: email validator

--- src/app/form/email/email.component.ts
+++ src/app/form/email/email.component.ts
@@ -1,10 +1,19 @@
 import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core'
-import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator, Validators, ValidationErrors } from '@angular/forms'
+import {
+  AbstractControlOptions,
+  ControlValueAccessor,
+  FormControl,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  Validator,
+  Validators,
+  ValidationErrors
+} from '@angular/forms'
 
 import { Subject } from 'rxjs'
 import { takeUntil } from 'rxjs/operators'
 
-import { createProvider, emailValidator } from '../../utils'
+import { AppValidators, createProvider } from '../../utils'
 
 @Component({
   providers: [
@@ -18,9 +27,18 @@ import { createProvider, emailValidator } from '../../utils'
 export class EmailComponent implements ControlValueAccessor, OnDestroy, Validator {
   @ViewChild('input') private readonly input: ElementRef<HTMLInputElement> | null = null
 
-  readonly email = new FormControl('', [Validators.required, emailValidator()])
+  readonly email: FormControl
 
+  private readonly controlOptions: Readonly<AbstractControlOptions> = {
+    updateOn: 'blur',
+    validators: [Validators.required, AppValidators.email()]
+  }
   private readonly finish = new Subject()
+
+
+  constructor() {
+    this.email = new FormControl('', this.controlOptions)
+  }
 
   registerOnChange(fn: any): void {
     this.email.valueChanges
--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -1,10 +1,60 @@
-import { ValidationErrors, ValidatorFn } from '@angular/forms';
+import { ValidationErrors, ValidatorFn } from '@angular/forms'
 
-export function emailValidator(): ValidatorFn {
-  return control => {
-    const error: Readonly<ValidationErrors> = {
-      email: { value: control.value }
-    }
-    return control.value.includes('@') ? null : error
-  }
+interface Lengths {
+  readonly domain: 189
+  readonly user: 64
 }
+
+export const AppValidators = (function appValidatorsFactory() {
+  const _atSignRegex = /@/g
+  const _maxLengths: Lengths = {
+    domain: 189,
+    user: 64
+  }
+
+  function email(): ValidatorFn {
+    return control => {
+      const email = control.value
+      const error: Readonly<ValidationErrors> = {
+        email: { value: email }
+      }
+      return _isEmail(email) ? null : error
+    }
+  }
+
+  function _checkDomain(domain: string): boolean {
+    return _checkLength(domain, 'domain') && _checkPeriod(domain)
+  }
+
+  function _checkLength(part: string, key: keyof Lengths): boolean {
+    return part.length <= _maxLengths[key]
+  }
+
+  function _checkPeriod(domain: string): boolean {
+    const period = '.'
+    if (!domain.includes(period)) {
+      return false
+    }
+    if (domain.startsWith(period) || domain.endsWith(period)) {
+      return false
+    }
+    return true
+  }
+
+  function _hasOneAtSign(email: string): boolean {
+    const atSigns = email.match(_atSignRegex)
+    return atSigns ? atSigns.length === 1 : false
+  }
+
+  function _isEmail(email: string): boolean {
+    if (!_hasOneAtSign(email)) {
+      return false
+    }
+    return email
+      .split(_atSignRegex)
+      .map((e, i) => (i === 0) ? _checkLength(e, 'user') : _checkDomain(e))
+      .reduce((a, b) => a && b)
+  }
+
+  return { email: email }
+})()
